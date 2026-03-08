abe02f0ef9f63fc146b1e91d1c115aa6f7a5eb83
vishalprajapati15 <vishalprajapati3266@gmail.com>
2026-03-08 18:27:22 +0530

    add ledger and transaction model

---
+++ src/models/ledger.model.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const ledgerSchema = new mongoose.Schema({
+    account:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Account",
+        required: [true, "Ledger entry must be associated with an account!!"],
+        index: true,
+        immutable: true
+    },
+    amount:{
+        type: Number,
+        required: [true, "Amount is required for a ledger entry!!"],
+        immutable: true
+    },
+    transaction:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Transaction",
+        required: [true, "Ledger entry must be associated with a transaction!!"],
+        index: true,
+        immutable: true
+    },
+    type:{
+        type: String,
+        enum:{
+            values: ["DEBIT", "CREDIT"],
+            message: "Ledger entry type must be either DEBIT or CREDIT!!"
+        },
+        required: [true, "Ledger entry type is required!!"],
+        immutable: true
+    }
+})
+
+function preventLedgerModification() {
+    throw new Error("Ledger entries cannot be modified or deleted!!");
+}
+
+ledgerSchema.pre("findOneAndDelete", preventLedgerModification);
+ledgerSchema.pre("findOneAndUpdate", preventLedgerModification);
+ledgerSchema.pre("updateOne", preventLedgerModification);
+ledgerSchema.pre("updateMany", preventLedgerModification);
+ledgerSchema.pre("update", preventLedgerModification);
+ledgerSchema.pre("remove", preventLedgerModification);
+
+const Ledger = mongoose.models.Ledger || mongoose.model("Ledger", ledgerSchema);
+
+export default Ledger;
---
+++ src/models/transaction.model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose"; 
+
+const transactionSchema = new mongoose.Schema({
+    account:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Account",
+        required: [true, "Transaction must be associated with an account!!"],
+        index: true
+    },
+    toAccount:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Account",
+        required: [true, "Transaction must be associated with a recipient account!!"],
+        index: true
+    },
+    status:{
+        type: String,
+        enum:{
+            values: ["PENDING", "COMPLETED", "FAILED", "REVERSED"],
+            message: "Status must be either PENDING, COMPLETED or FAILED or REVERSED!!"
+        },
+        default: "PENDING"
+    },
+    amount:{
+        type: Number,
+        required: [true, "Amount is required for a transaction!!"],
+        min: [0, "Amount must be a positive number!!"]
+    },
+    idempotencyKey:{
+        type: String,
+        required: [true, "Idempotency key is required for a transaction!!"],
+        unique: true,
+        index: true
+    }
+},{
+    timestamps: true
+});
+
+const Transaction = mongoose.models.Transaction || mongoose.model("Transaction", transactionSchema);
+
+export default Transaction;
